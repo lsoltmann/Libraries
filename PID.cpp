3a2e106c4023f7c5aede3b8bd162edce73a66699
Lars <lsoltmann@gmail.com>
2015-05-27 16:40:06 -0400

    Clean up

--- PID.cpp
+++ PID.cpp
@@ -1,88 +1,61 @@
 /*
  PID.cpp
- 
+
  Description: PID control functions
- 
- 
+
  Revision History
  Rev A - 26 April 2015 - Created and debugged
- 
+ Rev B - 27 May 2015 - clean up
+
  Author: Lars Soltmann
  */
-
-#include <stdint.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <math.h>
-#include <stdio.h>
-#include <iostream>
 
 #include "PID.h"
 
 CONTROL::CONTROL(){
-    p_error_sum=&error_sum;
-    p_error_previous=&error_previous;
-    p_t_previous=&t_previous;
-    *p_error_sum=0;
-    *p_error_previous=0;
-    *p_t_previous=0;
-    
+    error_sum=0;
+    error_previous=0;
+    t_previous=0;
 };
 
 CONTROL::~CONTROL(){
 };
 
 void CONTROL::reset(){
-    *p_error_sum=0;
-    *p_error_previous=0;
-    *p_t_previous=0;
+    error_sum=0;
+    error_previous=0;
+    t_previous=0;
 };
 
 float CONTROL::PID(float P, float I, float D, float target, float actual){
     // Get current time to find differential time element
     gettimeofday(&tv,NULL);
     t = 1000000 * tv.tv_sec + tv.tv_usec;
-    dt=t-*p_t_previous;
+    dt=t-t_previous;
 
     // Calculate current error
     error=target-actual;
 
     // Estimate derivative of error
-    _derivative=(error-*p_error_previous)/dt;
+    _derivative=(error-error_previous)/dt;
 
     // Estimate integral of error
-    *p_error_sum+=error;
-	if (*p_error_sum > I_UL) {
-        	*p_error_sum=I_UL;
-    	}
-    	if (*p_error_sum < I_LL) {
-        	*p_error_sum=I_LL;
-    	}
-    _integral=(*p_error_sum)*dt;
+    error_sum+=error;
 
-    // Apply integrator limits
-/*    if (_integral > I_UL)
-    {
-	_integral=I_UL;
+    // Apply integrator limits through error limiting
+    if (error_sum > I_UL) {
+    	error_sum=I_UL;
     }
-    if (_integral < I_LL)
-    {
-  	_integral=I_LL;
+    if (error_sum < I_LL) {
+       	error_sum=I_LL;
     }
-*/
+    _integral=(error_sum)*dt;
+
     // Calculate control input
     control_input=P*error+D*_derivative+I*_integral;
 
-    // Apply controller limits
-/*    if (control_input > CI_UL) {
-        control_input=CI_UL;
-    }
-    else if (control_input < CI_LL) {
-        control_input=CI_LL;
-    }
-*/
     // Safe time and error values for next loop;
-    *p_t_previous=t;
+    t_previous=t;
 
     return control_input;
 };
--- PID.h
+++ PID.h
@@ -1,23 +1,27 @@
 /*
  PID.h
- 
+
  Description: Header for PID library
- 
- 
+
  Revision History
  Rev A - 26 April 2015 - Created and debugged
- 
+ Rev B - 27 May 2015 - Clean up
+
  Author: Lars Soltmann
  */
 
 #ifndef PID_lib
 #define PID_lib
 
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
 
 #define I_UL 1000
 #define I_LL -1000
-//#define CI_UL 2.000 // servo pwm_ms max
-//#define CI_LL 1.000 // servo pwm_ms min
 
 class CONTROL
     {
@@ -26,20 +30,17 @@ class CONTROL
         ~CONTROL();
         float PID(float P, float I, float D, float target, float actual);
         void reset();
-        
+	float control_input;
+
     private:
         float _integral;
         float _derivative;
         float error;
-        float *p_error_sum;
         float error_sum;
-        float *p_error_previous;
         float error_previous;
-        float *p_t_previous;
         float t_previous;
         unsigned long t;
         float dt;
         struct timeval tv;
-        float control_input;
     };
 #endif
