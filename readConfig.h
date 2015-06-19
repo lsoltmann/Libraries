14c45b89f50ef753e740a519a3e0ff5ed5e6e57f
lsoltmann <lsoltmann@gmail.com>
2015-06-19 17:32:45 -0400

    Update readConfig.h

--- readConfig.h
+++ readConfig.h
@@ -23,9 +23,30 @@ class readConfig
 {
 public:
   readConfig(std::string name = "testconfig.txt");
-  void readfile();
-  int error_flag;
-  int output[17]; 
-  string line;
-  int counter;
+  int readfile();
+  int open_error;
+
+  int dataSampleRate;
+  int MS5805_active;
+  int MS5611_active;
+  int PPMdecode_active;
+  int AHRS_active;
+  int GPS_active;
+  int MS4515_active;
+  int SSC005D_active;
+  int ADC_active;
+  int RPM_active;
+  int IMU_active;
+  int gps_priority;
+  int ppm_priority;
+  int MS5611_priority;
+  int MS5805_priority;
+  int ahrs_priority;
+  int OUTPUT_TO_SCREEN;
+
+private:
+ ifstream params;
+ std::string file_name;
+ int output[17]; 
+ string line;
 }
