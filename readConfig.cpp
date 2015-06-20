f3472b49f45e3a8f9bd9d1270f0998feb1fe667e
lsoltmann <lsoltmann@gmail.com>
2015-06-20 10:19:52 -0400

    Added orientation option

--- readConfig.cpp
+++ readConfig.cpp
@@ -26,35 +26,40 @@ int readConfig::readfile(){
 		std::getline(params,line);
 		std::getline(params,line);
 		std::getline(params,line);
+		sscanf(line.c_str(),"%d",&output[1]);
 		std::getline(params,line);
-                sscanf(line.c_str(),"%d %d %d %d %d %d %d %d %d %d",&output[1],&output[2],&output[3],&output[4],&output[5],&output[6],&output[7],&output[8],&output[9],&output[10]);
+		std::getline(params,line);
+		std::getline(params,line);
+		std::getline(params,line);
+                sscanf(line.c_str(),"%d %d %d %d %d %d %d %d %d %d",&output[2],&output[3],&output[4],&output[5],&output[6],&output[7],&output[8],&output[9],&output[10],&output[11]);
 		std::getline(params,line);
                 std::getline(params,line);
                 std::getline(params,line);
 		std::getline(params,line);
-                sscanf(line.c_str(),"%d %d %d %d %d",&output[11],&output[12],&output[13],&output[14],&output[15]);
+                sscanf(line.c_str(),"%d %d %d %d %d",&output[12],&output[13],&output[14],&output[15],&output[16]);
 		std::getline(params,line);
                 std::getline(params,line);
                 std::getline(params,line);
-		sscanf(line.c_str(),"%d",&output[16]);
+		sscanf(line.c_str(),"%d",&output[17]);
 	
 		dataSampleRate=output[0];
-		MS5805_active=output[1];
-		MS5611_active=output[2];
-  		PPMdecode_active=output[3];
-  		AHRS_active=output[4];
-  		GPS_active=output[5];
-  		MS4515_active=output[6];
-  		SSC005D_active=output[7];
+		sys_orientation=output[1];
+		MS5805_active=output[2];
+		MS5611_active=output[3];
+  		PPMdecode_active=output[4];
+  		AHRS_active=output[5];
+  		GPS_active=output[6];
+  		MS4515_active=output[7];
+  		SSC005D_active=output[8];
   		ADC_active=output[8];
-  		RPM_active=output[9];
-  		IMU_active=output[10];
-  		gps_priority=output[11];
-  		ppm_priority=output[12];
-  		MS5611_priority=output[13];
-  		MS5805_priority=output[14];
-  		ahrs_priority=output[15];
-  		OUTPUT_TO_SCREEN=output[16];
+  		RPM_active=output[10];
+  		IMU_active=output[11];
+  		gps_priority=output[12];
+  		ppm_priority=output[13];
+  		MS5611_priority=output[14];
+  		MS5805_priority=output[15];
+  		ahrs_priority=output[16];
+  		OUTPUT_TO_SCREEN=output[17];
 	}
 	else{
 		open_error++;
@@ -78,6 +83,7 @@ int readConfig::readfile(){
       	cout << output[14] << "\n";
      	cout << output[15] << "\n";
      	cout << output[16] << "\n";
+     	cout << output[17] << "\n";
 */
 	params.close();
 }
